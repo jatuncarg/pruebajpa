011b9a044c4bf5c675545383160a1413b380bb57
unknown <SUITE@SUITECE205-SERV.cibertec.edu.pe>
2024-09-17 18:44:07 -0500

    Mantenimiento de Prod y se agregó el Login

---
+++ src/main/java/com/ciberfama/app/FrmLogin.java
@@ -0,0 +1,83 @@
+package com.ciberfama.app;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+
+public class FrmLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JTextField txtClave;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmLogin frame = new FrmLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 146);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.setBounds(324, 29, 89, 23);
+		contentPane.add(btnIngresar);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(122, 30, 161, 20);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblUsuario = new JLabel("Usuario :");
+		lblUsuario.setBounds(10, 33, 102, 14);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblClave = new JLabel("Clave :");
+		lblClave.setBounds(10, 64, 102, 14);
+		contentPane.add(lblClave);
+		
+		txtClave = new JTextField();
+		txtClave.setColumns(10);
+		txtClave.setBounds(122, 61, 161, 20);
+		contentPane.add(txtClave);
+		
+	}
+
+	
+	
+	
+	
+	
+}
